2b21f3bac8092df4dbe54e5c4f7aec8fd2cbda6b
harshal-0101 <mahajanharshalnitin@gmail.com>
2025-12-23 22:17:40 +0530

    Created Navbar

--- components/Application/Website/Header.jsx
+++ components/Application/Website/Header.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react';
+import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin, FaUser, FaShoppingBag, FaHeart, FaSearch, FaBars, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import "./Header.css";
+import Image from "next/image";
+import logo from "../../images/PUSTAK-MAZA-LOGO.png";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Header
+  return (
+    <div className="header-container">
+      <div className="navbar">
+
+        {/* TOP BAR */}
+        <div className="top-container">
+          <div className="contactnumber">+91 9876543210</div>
+          <div className="social-media">
+            <Link href="#"><FaInstagram /></Link>
+            <Link href="#"><FaFacebook /></Link>
+            <Link href="#"><FaTwitter /></Link>
+            <Link href="#"><FaLinkedin /></Link>
+          </div>
+        </div>
+
+        <hr />
+
+        {/* MAIN NAV */}
+        <div className="main-nav">
+          <div className="logo">
+            <Image src={logo} alt="logo" className="logoimg" />
+          </div>
+
+          <div className="search-book">
+            <input type="search" placeholder="Search books.." />
+            <button className="SearchBTN"><FaSearch /></button>
+          </div>
+
+          <div className="lists desktop-only">
+            <ul>
+              <li><Link className="listbtn" href="#"><FaUser /> <span className='listbtntext'>Account</span></Link></li>
+              <li><Link className="listbtn" href="#"><FaShoppingBag /> <span className='listbtntext'>Cart</span></Link></li>
+              <li><Link className="listbtn" href="#"><FaHeart /> <span className='listbtntext'>Wishlist</span></Link></li>
+            </ul>
+          </div>
+
+          {/* HAMBURGER */}
+          <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </div>
+        </div>
+
+        {/* PAGE MENU */}
+        <div className={`page-list ${menuOpen ? "active" : ""}`}>
+          <ul>
+            <li><Link href="#">HOME</Link></li>
+            <li><Link href="#">ABOUT US</Link></li>
+            <li><Link href="#">BOOKS</Link></li>
+            <li><Link href="#">NEW RELEASE</Link></li>
+            <li><Link href="#">CONTACT US</Link></li>
+            <li><Link href="#">BLOG</Link></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
