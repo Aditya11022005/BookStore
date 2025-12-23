213c3ae0be3bbd4f85c22f39a641d75ecafe231f
harshal-0101 <mahajanharshalnitin@gmail.com>
2025-12-24 00:59:02 +0530

    Developed login and register page

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import "./login.css"
+import Image from "next/image";
+import loginimg from "../images/pustakma.png";
+import Link from 'next/link';
 
 const LoginPage = () => {
   return (
-    <div>LogiPage</div>
+    <div>
+      <div className="login-container">
+        <div className="login-img">
+          <Image src={loginimg} alt="loginimage" className="loginimage"/>
+        </div>
+        <div className="login-form">
+          <h2>Login</h2>
+          <form action="">
+            <label htmlFor="username">Email</label><br />
+            <input type="email" /><br />
+            <span className='errormsg'>enter valid email</span> <br />
+            <label htmlFor="password">Password</label><br />
+            <input type="password" /><br />
+            <span className='errormsg'>enter valid password</span> <br />
+            <button type="submit">Login</button>
+          </form>
+          <div className="moreOptionforForm">
+            <p>Don't have an account?<Link href="/auth/register"> Register</Link></p>
+            <span>OR</span>
+            <p><a href="/auth/forgotpassword">Forgot Password?</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/(root)/auth/register/page.jsx
+++ app/(root)/auth/register/page.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import "./Register.css"
+import Image from "next/image";
+import loginimg from "../images/pustakma.png";
+import Link from 'next/link';
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div>
+         <div className="login-container">
+           <div className="login-img">
+             <Image src={loginimg} alt="loginimage" className="loginimage"/>
+           </div>
+           <div className="login-form">
+             <h2>Login</h2>
+             <form action="">
+              <label htmlFor="username">User Name</label><br />
+                <input type="text" /><br />
+                <span className='errormsg'>enter valid user name</span> <br />
+              
+               <label htmlFor="username">Email</label><br />
+               <input type="email" /><br />
+               <span className='errormsg'>enter valid email</span> <br />
+               <label htmlFor="password">Password</label><br />
+               <input type="password" /><br />
+               <span className='errormsg'>enter valid password</span> <br />
+               <button type="submit">Register</button>
+             </form>
+             <div className="moreOptionforForm">
+               <p>You have an account?<Link href="/auth/login"> Login</Link></p>
+             </div>
+           </div>
+         </div>
+       </div>
   )
 }
 
