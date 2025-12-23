f6d7213cb55ec94e01da9fa0998ee8bd9b847e31
Aditya11022005 <pandeaditya120@gmail.com>
2025-12-23 23:11:30 +0530

    admin sidebar complite

--- app/(root)/(admin)/admin/layout.jsx
+++ app/(root)/(admin)/admin/layout.jsx
@@ -1,8 +1,14 @@
+import AppSidebar from '@/components/Application/Admin/AppSidebar'
+import { SidebarProvider } from '@/components/ui/sidebar'
 import React from 'react'
 
 const layout = ({ children }) => {
   return (
+    <SidebarProvider>
+      <AppSidebar/>
     <div>{children}</div>
+
+    </SidebarProvider>
   )
 }
 
